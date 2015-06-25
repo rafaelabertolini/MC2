7571b482616e2691dd20aeaab5a34f1de3624f25
Rafaela S Bertolini <bertolinifr@gmail.com>
2015-06-25 12:40:51 -0300

    Com os novos icons

---
+++ MCS/CustomTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTabBarController.swift
+//  MCS
+//
+//  Created by Rafaela dos Santos Bertolini on 25/06/15.
+//  Copyright (c) 2015 com.TBG. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+}
