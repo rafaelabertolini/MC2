5c8fae0a1eb51994f10fb2f992c4527c159bfccc
Karen Garcia <karenkgs@gmail.com>
2015-06-25 15:08:40 -0300

    Página Inicial

--- MCS/InitialViewController.swift
+++ MCS/InitialViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class InitialViewController: UIViewController {
 
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet weak var problemaView: UIView!
+    
+    @IBOutlet weak var prosContrasView: UIView!
+    
+    @IBOutlet weak var planosView: UIView!
+    
+    
+    @IBOutlet weak var metasView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.viewWithTag(01)?.backgroundColor = UIColor(red: 52, green: 105, blue: 102, alpha: 1)
+        contentView.layer.cornerRadius = 35
+        problemaView.layer.cornerRadius = 35
+        prosContrasView.layer.cornerRadius = 35
+        planosView.layer.cornerRadius = 35
+        metasView.layer.cornerRadius = 35
         // Do any additional setup after loading the view, typically from a nib.
     }
 
