21bf51980db62b58aee586eac586ff6369df89cc
Karen Garcia <karenkgs@gmail.com>
2015-06-25 20:52:40 -0300

    Novas imagens e botões

--- MCS/InitialViewController.swift
+++ MCS/InitialViewController.swift
@@ -13,13 +13,33 @@ class InitialViewController: UIViewController {
     @IBOutlet weak var contentView: UIView!
     
     @IBOutlet weak var problemaView: UIView!
+    @IBOutlet weak var problemaButton: UIButton!
     
     @IBOutlet weak var prosContrasView: UIView!
+    @IBOutlet weak var prosContrasButton: UIButton!
     
     @IBOutlet weak var planosView: UIView!
-    
+    @IBOutlet weak var planosButton: UIButton!
     
     @IBOutlet weak var metasView: UIView!
+    @IBOutlet weak var metasButton: UIButton!
+    
+    @IBAction func problemaAction(sender: UIButton) {
+        pressionarView()
+    }
+    
+    @IBAction func prosContrasAction(sender: UIButton) {
+        pressionarView()
+    }
+    
+    
+    @IBAction func planosAction(sender: UIButton) {
+        pressionarView()
+    }
+    
+    @IBAction func metasAction(sender: UIButton) {
+        pressionarView()
+    }
     
     
     override func viewDidLoad() {
@@ -30,7 +50,16 @@ class InitialViewController: UIViewController {
         prosContrasView.layer.cornerRadius = 35
         planosView.layer.cornerRadius = 35
         metasView.layer.cornerRadius = 35
+        
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func pressionarView(){
+        let tapAlert = UIAlertController(title: "Tapped", message: "You just tapped the tap view", preferredStyle: UIAlertControllerStyle.Alert)
+        tapAlert.addAction(UIAlertAction(title: "OK", style: .Destructive, handler: nil))
+        self.presentViewController(tapAlert, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
