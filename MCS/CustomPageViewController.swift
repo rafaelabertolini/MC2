09ebb6066d43e3e063d9391a454838adfeb0bb51
Rafaela S Bertolini <bertolinifr@gmail.com>
2015-06-25 21:50:06 -0300

    Novas classes do tutorial

---
+++ MCS/CustomPageViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CustomPageViewController.swift
+//  MCS
+//
+//  Created by Rafaela dos Santos Bertolini on 25/06/15.
+//  Copyright (c) 2015 com.TBG. All rights reserved.
+//
+
+import UIKit
+
+class CustomPageViewController: UIViewController,BWWalkthroughPage {
+    
+    @IBOutlet var imageView:UIImageView?
+    @IBOutlet var titleLabel:UILabel?
+    @IBOutlet var textLabel:UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: BWWalkThroughPage protocol
+    
+    func walkthroughDidScroll(position: CGFloat, offset: CGFloat) {
+        var tr = CATransform3DIdentity
+        tr.m34 = -1/500.0
+        
+        titleLabel?.layer.transform = CATransform3DRotate(tr, CGFloat(M_PI) * (1.0 - offset), 1, 1, 1)
+        textLabel?.layer.transform = CATransform3DRotate(tr, CGFloat(M_PI) * (1.0 - offset), 1, 1, 1)
+        
+        var tmpOffset = offset
+        if(tmpOffset > 1.0){
+            tmpOffset = 1.0 + (1.0 - tmpOffset)
+        }
+        imageView?.layer.transform = CATransform3DTranslate(tr, 0 , (1.0 - tmpOffset) * 200, 0)
+    }
+    
+}
