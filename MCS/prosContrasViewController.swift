054806f5cc94762f8bfae85e19891f58f672dabd
Karen Garcia <karenkgs@gmail.com>
2015-06-30 16:16:36 -0300

    Nova tabela

--- MCS/prosContrasViewController.swift
+++ MCS/prosContrasViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 
-class prosContrasViewController: UIViewController {
+class ProsContrasViewController: UIViewController, UITableViewDataSource {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-//        self.navigationController?.navigationBar.hidden = false
-        self.navigationController?.setNavigationBarHidden = false
+        self.navigationController?.navigationBar.hidden = false
+        
+        println(self.navigationController)
+        
+//        self.navigationController?.setNavigationBarHidden = false
     }
     
+    
+    // MARK: UITableViewDataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0;
+        //return notes.count
+    }
+    
+    func tableView(tableView: UITableView,  cellForRowAtIndexPath  indexPath: NSIndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
+        //let reflection = notes[indexPath.row]
+        //cell.textLabel!.text = reflection.valueForKey("note") as! String?
+        return cell
+    }
+
 }
