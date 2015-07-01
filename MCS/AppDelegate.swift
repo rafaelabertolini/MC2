99ffd7157ca20bff3ebe58b0bd59b7ffe7e592da
Rafaela S Bertolini <bertolinifr@gmail.com>
2015-07-01 15:55:49 -0300

    Com a tab bar ajustada

--- MCS/AppDelegate.swift
+++ MCS/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        UITabBar.appearance().tintColor = selectedTintColor
+        UITabBar.appearance().tintColor = tintColor
         
         var tabBarAppearance = UITabBar.appearance()
         
