1ad9b8dc0ab9276977340488a03b2653c01f2d2d
Karen Garcia <karenkgs@gmail.com>
2015-07-02 16:19:46 -0300

    Organização do código e Watch.

---
+++ MCS WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  MCS WatchKit Extension
+//
+//  Created by Karen Garcia dos Santos on 02/07/15.
+//  Copyright (c) 2015 com.TBG. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
