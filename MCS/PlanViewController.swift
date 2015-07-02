19de91f20842aa3e98debd21541dc36ab9989a02
Rafaela S Bertolini <bertolinifr@gmail.com>
2015-07-02 14:28:05 -0300

    arruma ae karen

--- MCS/PlanViewController.swift
+++ MCS/PlanViewController.swift
@@ -8,83 +8,44 @@
 
 import UIKit
 
-struct Activity {
-    var actv: String
-}
-
-struct Situation {
-    var sit: String
-}
 
 
-class PlanViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class PlanViewController: UIViewController {
 
+    
     
     @IBOutlet weak var mainObj: UITextView!
-    @IBOutlet weak var tableView1: UITableView!
-    @IBOutlet weak var tableView2: UITableView!
     
     
+    @IBOutlet weak var act1: UIButton!
+    @IBOutlet weak var act2: UIButton!
+    @IBOutlet weak var act3: UIButton!
+    @IBOutlet weak var act4: UIButton!
+    @IBOutlet weak var act5: UIButton!
+    @IBOutlet weak var act6: UIButton!
     
-    var activities: [Activity] = [
+    @IBOutlet weak var sit1: UIButton!
+    @IBOutlet weak var sit2: UIButton!
+    @IBOutlet weak var sit3: UIButton!
+    @IBOutlet weak var sit4: UIButton!
+    @IBOutlet weak var sit5: UIButton!
+    @IBOutlet weak var sit6: UIButton!
+    @IBOutlet weak var sit7: UIButton!
+    @IBOutlet weak var sit8: UIButton!
+    @IBOutlet weak var sit9: UIButton!
+    @IBOutlet weak var sit10: UIButton!
     
-        Activity(actv: "ola")
     
-    ]
-    
-    
-    var situations: [Situation] = [
-        
-        Situation(sit: "alo")
-        
-    ]
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
-        tableView1.backgroundColor = UIColor(red: 38/255, green: 87/255, blue: 83/255, alpha: 1.0)
-        tableView1.tableFooterView = UIView(frame:CGRectZero)
-        
-        tableView2.backgroundColor = UIColor(red: 38/255, green: 87/255, blue: 83/255, alpha: 1.0)
-        tableView2.tableFooterView = UIView(frame:CGRectZero)
-        
     }
 
    
     
-    //MARK: Table View Data Source
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            
-        if(tableView.tag == 5){
-            return activities.count
-        } else {
-            return situations.count
-        }
-    }
-    
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        if(tableView.tag == 5){
-        
-            var cell1 = tableView.dequeueReusableCellWithIdentifier("Cell1") as! ActivityTableViewCell
-            cell1.actv.text = activities[indexPath.row].actv
-            cell1.backgroundColor = UIColor(red: 131/255, green: 186/255, blue: 184/255, alpha: 1.0)
-            
-            
-            return cell1
-            
-        } else {
-        
-            var cell2 = tableView.dequeueReusableCellWithIdentifier("Cell2") as! SituationTableViewCell
-            cell2.sit.text = situations[indexPath.row].sit
-            cell2.backgroundColor = UIColor(red: 131/255, green: 186/255, blue: 184/255, alpha: 1.0)
-            
-            return cell2
-        }
-        
-    }
-
+   
 }
