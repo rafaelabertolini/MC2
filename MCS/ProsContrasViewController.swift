428a73ba7389d19ac758fda6ba8ef4a32b45ce72
Karen Garcia <karenkgs@gmail.com>
2015-07-02 14:07:31 -0300

    Pros e Contras pronto

--- MCS/ProsContrasViewController.swift
+++ MCS/ProsContrasViewController.swift
@@ -14,19 +14,25 @@ class ProsContrasViewController: UIViewController, UITableViewDataSource {
     var pros = [Pro]()
     var cons = [Con]()
     
+    var currentEntity = "Pro"
+    
     @IBOutlet weak var prosOrConsSegmentedControl: UISegmentedControl!
     
     
     @IBAction func selectedProsOrCons(sender: UISegmentedControl) {
+            
         switch prosOrConsSegmentedControl.selectedSegmentIndex{
             case 0:
-                self.populateTable("Pro")
+                self.currentEntity = "Pro"
+                tableView.reloadData()
             break;
             case 1:
-                self.populateTable("Con")
+                self.currentEntity = "Con"
+                tableView.reloadData()
             break;
             default:
-                self.populateTable("Pro")
+                self.currentEntity = "Pro"
+                tableView.reloadData()
             break;
         }
     }
@@ -81,7 +87,7 @@ class ProsContrasViewController: UIViewController, UITableViewDataSource {
         let appDelegate =  UIApplication.sharedApplication().delegate as! AppDelegate
         let managedContext = appDelegate.managedObjectContext!
         
-        let entity =  NSEntityDescription.entityForName("Pro",    inManagedObjectContext:    managedContext)
+        let entity =  NSEntityDescription.entityForName("Pro", inManagedObjectContext:    managedContext)
         let pro = NSManagedObject(entity: entity!, insertIntoManagedObjectContext:managedContext) as! Pro
         
         pro.setValue(note, forKey: "note")
@@ -115,27 +121,64 @@ class ProsContrasViewController: UIViewController, UITableViewDataSource {
         
     }
     
-    func populateTable(entidade: String){
-        
-    }
-    
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.hidden = false
+        
+        let appDelegate =  UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext!
+        
+        let fetchProsRequest = NSFetchRequest(entityName:"Pro")
+        
+        let fetchConsRequest = NSFetchRequest(entityName: "Con")
+        
+        var error: NSError?
+        
+        let fetchedConsResults = managedContext.executeFetchRequest(fetchConsRequest, error: &error)
+        if let results = fetchedConsResults {
+            cons = results as! Array<Con>
+        } else {
+            println("Could not fetch \(error), \(error!.userInfo)")
+        }
+        
+        
+        let fetchedProResults = managedContext.executeFetchRequest(fetchProsRequest, error: &error)
+        if let resultsPro = fetchedProResults {
+            pros = resultsPro as! Array<Pro>
+        } else {
+            println("Could not fetch \(error), \(error!.userInfo)")
+        }
+
+        
     }
+    
+    
     
     
     // MARK: UITableViewDataSource
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if(self.currentEntity == "Pro"){
+            return pros.count
+        } else if(self.currentEntity == "Con"){
+            return cons.count
+        }
         return 0;
-        //return notes.count
     }
     
     func tableView(tableView: UITableView,  cellForRowAtIndexPath  indexPath: NSIndexPath) -> UITableViewCell {
         let cell =  tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
-        //let reflection = notes[indexPath.row]
-        //cell.textLabel!.text = reflection.valueForKey("note") as! String?
+        if(self.currentEntity == "Pro"){
+            let pro = pros[indexPath.row]
+            cell.textLabel!.text = pro.valueForKey("note") as! String?
+        } else if(self.currentEntity == "Con") {
+            let con = cons[indexPath.row]
+            cell.textLabel!.text = con.valueForKey("note") as! String?
+        }
+        
+        cell.backgroundColor = UIColor(red: 131/255, green: 186/255, blue: 184/255, alpha: 1.0)
+        
         return cell
     }
+
     
 }
